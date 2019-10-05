b3691f8e78976fb2450a2312507b4ed91c3450d6
matearealies <matearealies@gmail.com>
2019-10-05 01:53:30 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/matearealies/precache-manifest.708c1e69a9bc13abd2bdc17e1ac103e2.js"
+  "/matearealies/precache-manifest.53f02745c065207ed31d385874927543.js"
 );
 
 self.addEventListener('message', (event) => {
