61e1856ea1838d827910d9811bc74119258a254b
matearealies <matearealies@gmail.com>
2019-10-05 01:03:11 -0400

    Updates

---
+++ precache-manifest.708c1e69a9bc13abd2bdc17e1ac103e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0787fa7815a6e4f83d480b52743b2be4",
+    "url": "/matearealies/index.html"
+  },
+  {
+    "revision": "edef5755284c1b6ebe1d",
+    "url": "/matearealies/static/css/main.17bcefad.chunk.css"
+  },
+  {
+    "revision": "fb9ae721ddc211a58252",
+    "url": "/matearealies/static/js/2.512f52e0.chunk.js"
+  },
+  {
+    "revision": "edef5755284c1b6ebe1d",
+    "url": "/matearealies/static/js/main.36269115.chunk.js"
+  },
+  {
+    "revision": "7ea26051de94efe3403e",
+    "url": "/matearealies/static/js/runtime~main.73d2f3dc.js"
+  }
+]);
